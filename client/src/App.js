edd21651c647a7adb2ba674450ce815d7fb45e7b
Alex Hoyer <alexhoyer22@gmail.com>
2022-03-27 15:57:56 -0700

    Added Edit Profile component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
+import EditProfile from './components/profile-forms/EditProfile'
 import PrivateRoute from './components/routing/PrivateRoute'
 //Redux
 import {Provider} from 'react-redux';
@@ -50,6 +51,12 @@ const App = () => {
             <Route exact path="/create-profile" element={
                 <PrivateRoute>
                   <CreateProfile />
+                </PrivateRoute>
+              } 
+            />
+            <Route exact path="edit-profile" element={
+                <PrivateRoute>
+                  <EditProfile />
                 </PrivateRoute>
               } 
             />
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions'
 
 const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -16,7 +17,7 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loadin
             {/* If user exists, show the user name */}
             <i className="fas fa-user"></i> Welcome {user && user.name}
         </p>
-        {profile !== null ? <Fragment>has</Fragment> : <Fragment>
+        {profile !== null ? <Fragment><DashboardActions/></Fragment> : <Fragment>
                 <p>You have not yet setup a profile, please add some info</p>
                 <Link to='/create-profile' className='btn btn-primary my-1'>
                     Create Profile
